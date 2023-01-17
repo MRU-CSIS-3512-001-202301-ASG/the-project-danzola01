d6e533e8c15f137c6410097717bcb8c047500e2a
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-01-17 19:37:39 +0000

    login form, added simle formatting

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -5,16 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Login Page</title>
 </head>
 
 <body>
+    <h1>Welcome Back!</h1>
     <form action="/login" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password"><br>
-        <input type="submit" value="Login">
+        <div class="center">
+            <img src="images/user.svg" alt="user icon">
+            <input type="text" id="username" name="username" placeholder="Username"><br>
+        </div>
+
+        <div class="center">
+            <img src="images/lock.svg" alt="lock icon">
+            <input type="password" id="password" name="password" placeholder="Password"><br>
+        </div>
+
+        <div class="center">
+            <input type="submit" value="Login">
+        </div>
     </form>
 </body>
 
