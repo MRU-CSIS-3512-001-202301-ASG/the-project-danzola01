b6b41bb2ab2c47afc681f75fb3b39f6875aee2c7
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-01-17 19:38:11 +0000

    updated connection info

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,12 +1,12 @@
 <?php
 // Define database connection constants
-$servername = "34.28.3.253";
+$servername = "34.68.130.132";
 $username = "root";
 $password = "comp3512";
 $dbname = "travel";
 
 // Create connection
- $conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
