ed31bdde4a7017d4095cbe602dbbe8893f05d91d
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-01-19 20:49:02 +0000

    cleaned up db config

--- htdocs/config.php
+++ htdocs/config.php
@@ -10,33 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
 }
-
-function insert_into_users($conn, $userID, $firstName, $lastName, $city, $region, $country, $postal, $phone, $email, $privacy)
-{
-    $sql = "INSERT INTO users (UserID, FirstName, LastName, City, Region, Country, Postal, Phone, Email, Privacy)
-    VALUES ('$userID', '$firstName', '$lastName', '$city', '$region', '$country', '$postal', '$phone', '$email', '$privacy')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-// testing the function
-$userID = "123";
-$firstName = "John";
-$lastName = "Doe";
-$city = "Toronto";
-$region = "Ontario";
-$country = "Canada";
-$postal = "M5H 2N2";
-$phone = "416-123-4567";
-$email = "john@doe.com";
-$privacy = "1";
-
-insert_into_users($conn, $UserID, $FirstName, $LastName, $City, $Region, $Country, $Postal, $Phone, $Email, $Privacy);
-
-
-$conn->close();
