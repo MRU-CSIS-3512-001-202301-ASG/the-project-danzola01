3f14b8e308924ab583e22d3b37e785d2c5ed04d1
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-01-25 04:13:36 +0000

    partials created

---
+++ htdocs/views/partials/head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+
+    <!-- Pico CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+
+    <!-- CSS per resource -->
+    <?php foreach ($stylesheets as $sheet) : ?>
+    <link rel="stylesheet" href="css/<?= $sheet ?>">
+    <?php endforeach; ?>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+
+    <!-- Page title -->
+    <title><?= $page_title ?></title>
+</head>
+
+<body>
