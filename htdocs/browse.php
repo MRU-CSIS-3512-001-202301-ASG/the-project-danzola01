3b0db99a5240e348bfd5ff1e51712ebcac02c133
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-01-26 22:10:59 +0000

    Handlle new rating value

--- htdocs/browse.php
+++ htdocs/browse.php
@@ -15,8 +15,13 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("Location: admin.php");
 }
 
-// If the user is logged in, displat the browse page
+// If the user is logged in, display the browse page
 else {
     require 'helpers.php';
     require 'views/browse.view.php';
+}
+
+// receive the new rating from the form and update the value in the $post array
+if (isset($_POST['new_rating'])) {
+    $post['rating'] = $_POST['new_rating'];
 }
