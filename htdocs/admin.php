02ca3c32a8f5ab9ed8f5440b7faea1c973f20931
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-03-08 21:10:20 +0000

    admin page now uses server side validation

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -1,11 +1,13 @@
 <?php
-
+// Start the PHP session
 session_start();
 
+// Link to the stylesheet
 $stylesheets = [
     "style.css"
 ];
 
+// Set the page title
 $page_title = "Admin Portal";
 
 // Connect to the database
@@ -18,10 +20,9 @@ $config = require 'database/config.php';
 $dbClass = new DatabaseHelper($config);
 $dbh = $dbClass->getDb();
 
-$invalid_username = "";
-$invalid_password = "";
-
 $errors = [];
+$invalid_username = null;
+$invalid_password = null;
 
 // Arriving here via GET -> display the form
 if ($_SERVER['REQUEST_METHOD'] === "GET") {
@@ -30,40 +31,43 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
 
 // Arriving here via POST -> process the form
 else if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
     // Ensure that the username is not empty
     if (strlen($_POST['username']) === 0) {
         $errors['username'] = "A username is required!";
-        $invalid_username = "aria-invalid='true'";
+        $invalid_username = "true";
     }
 
-    if (empty($_POST['password']) === 0) {
+    // Ensure that the password is not empty
+    if (strlen($_POST['password']) === 0) {
         $errors['password'] = "A password is required!";
-        $invalid_password = "aria-invalid='true'";
+        $invalid_password = "true";
     }
 
     // Validate the username and password
     $username = $_POST['username'];
     $retrieved_digest = get_digest($dbh, $username);
 
-    // If array is empty, the username does not exist
-    if (!empty($retrieved_digest) && password_verify($_POST['password'], $retrieved_digest[0]['digest']) === false) {
-        $errors[] = "You have entered an invalid username or password!";
+    if (empty($retrieved_digest)) {
+        $errors['validation'] = "You have entered an invalid username or password!";
+    } else {
+        if (password_verify($_POST['password'], $retrieved_digest[0]) === false) {
+            $errors['validation'] = "You have entered an invalid username or password!";
+        }
     }
-
 
     // If there are no errors, redirect to the admin page, otherwise display the form again
     if (empty($errors)) {
 
         $_SESSION['logged_in'] = true;
         $_SESSION['username'] = $_POST['username'];
-        // TODO: If the user authenticates correctly, the login status is saved using PHP session state, and the form redirects to the Browse/Filter Page.
         header("Location: browse.php");
     } else {
         require 'views/admin.view.php';
     }
 }
 
-// Arriving here via any other method -> redirect to the form
+// Arriving here via any other method (just in case) -> redirect to the form
 else {
     header("Location: admin.php");
 }
