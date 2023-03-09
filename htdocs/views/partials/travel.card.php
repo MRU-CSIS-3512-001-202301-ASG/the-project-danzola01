22e3b7eddfaf597f83b107a4c612cdeaf2151f0f
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-03-09 21:25:33 +0000

    Small formatting change

--- htdocs/views/partials/travel.card.php
+++ htdocs/views/partials/travel.card.php
@@ -1,7 +1,8 @@
 <tr>
     <!-- Image -->
     <th scope="row">
-        <img class="center" src="<?= cloudinary_src($post['Path']) ?>" alt="Image from <?= $post['CityName'] . ", " . $post['CountryName'] ?>">
+        <img class="center" src="<?= cloudinary_src($post['Path']) ?>"
+            alt="Image from <?= $post['CityName'] . ", " . $post['CountryName'] ?>">
     </th>
 
     <!-- Country -->
@@ -23,7 +24,7 @@
     <td>
         <form class="margin_0" action="../../browse.php">
             <!-- Select new rating -->
-            <select class="select_new_rating margin_0" id="<?= "new_rating_" . $image_id ?>" name="<?= "new_rating_" . $image_id ?>">
+            <select class="select_new_rating margin_0" id="new_rating" name="new_rating">
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -35,7 +36,7 @@
             <input type="hidden" name="image_id" value="<?= $image_id ?>">
 
             <!-- Submit new rating -->
-            <input class="submit_new_rating contrast" type="submit">
+            <button type="submit" name="submit_new_rating" class="contrast margin_0">Submit</button>
         </form>
     </td>
 </tr>
