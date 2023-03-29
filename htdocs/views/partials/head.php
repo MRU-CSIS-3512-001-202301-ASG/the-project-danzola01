7e3418fcad7779368632e6c3aeecef4bd6572872
Diego Anzola <43018673+danzola01@users.noreply.github.com>
2023-03-29 03:23:33 +0000

    added nav with logout

--- htdocs/views/partials/head.php
+++ htdocs/views/partials/head.php
@@ -10,7 +10,7 @@
 
     <!-- CSS per resource -->
     <?php foreach ($stylesheets as $sheet) : ?>
-        <link rel="stylesheet" href="css/<?= $sheet ?>">
+    <link rel="stylesheet" href="css/<?= $sheet ?>">
     <?php endforeach; ?>
 
     <!-- Favicon -->
@@ -21,3 +21,20 @@
 </head>
 
 <body>
+    <!-- Nav -->
+    <nav class="container-fluid">
+        <ul>
+            <li><a href="./" class="contrast bold ">We Plan, You Travel ✈️</a></li>
+        </ul>
+        <!-- If the current page is browse.php, display -->
+        <?php if (basename($_SERVER['PHP_SELF']) == 'browse.php') : ?>
+        <ul>
+            <li>
+                <!-- logout button -->
+                <form action=" browse.php" method="post" class="margin_5">
+                    <button type="submit" name="logout" class="contrast">Logout</button>
+                </form>
+            </li>
+        </ul>
+        <?php endif; ?>
+    </nav>
